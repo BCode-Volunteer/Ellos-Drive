069bc42a1c777f42e6e90c49641fed282c22aca0
rodriguesxxx <rodriguesx.daniel89@gmail.com>
2024-11-01 12:06:52 -0300

    [TODOs] Criando próximas tarefas

--- app/Http/Controllers/DriveController.php
+++ app/Http/Controllers/DriveController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+//TODO: Integração com o google
+//TODO: Listar pastas e arquivos
+//TODO: Download de determinado arquivo
+
 class DriveController extends Controller
 {
     public function index()
